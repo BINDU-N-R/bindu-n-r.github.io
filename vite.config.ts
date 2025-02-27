e2e5d992546b3e194304ac67682b51491cc3d158
BINDU-N-R <bindu6433@gmail.com>
2025-02-27 18:20:28 +0530

    Fix github pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bindu-n-r.github.io/',
+  base: '/bindu-n-r.github.io/', // this is important for github pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
